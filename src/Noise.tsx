704b2424c6aaf1b516f8f473d6e4be17d2541925
Moniet <moniet.sawhney@gmail.com>
2023-11-06 18:37:27 +0100

    ✨apply css module styles

--- src/Noise.tsx
+++ src/Noise.tsx
@@ -1,18 +1,27 @@
-import { ReactNode, forwardRef } from "react"
+import { CSSProperties, ReactNode, forwardRef } from "react"
 import useNoise from "./useNoise"
 
-import "./index.css"
+import styles from "./index.module.css"
 
 export type NoiseProps = {
   children?: ReactNode
   className?: string
   noiseKey?: string
   isAnimated?: boolean
+  style?: CSSProperties
+  opacity?: number
 }
 
 const Noise = forwardRef<HTMLDivElement, NoiseProps>(
   (
-    { children, className = "", noiseKey = "default", isAnimated = false },
+    {
+      children,
+      className = "",
+      noiseKey = "default",
+      isAnimated = false,
+      style = {},
+      opacity = 1
+    },
     ref
   ) => {
     const canvasRef = useNoise(noiseKey)
@@ -20,12 +29,20 @@ const Noise = forwardRef<HTMLDivElement, NoiseProps>(
     return (
       <div
         ref={ref}
+        style={style}
         data-noise="container"
-        className={className}
         data-noise-anim={isAnimated}
+        className={`${styles.container} ${className}`}
       >
-        <canvas ref={canvasRef} data-noise="canvas"></canvas>
-        <div data-noise="children">{children}</div>
+        <canvas
+          ref={canvasRef}
+          data-noise="canvas"
+          className={styles.canvas}
+          style={{ "--opacity": opacity } as any}
+        ></canvas>
+        <div data-noise="children" className={styles.children}>
+          {children}
+        </div>
       </div>
     )
   }
